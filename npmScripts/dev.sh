9c3af8c920ce2bf2172b8034b665ca137b6b519f
junshen <redwolf85911@gmail.com>
2017-08-05 22:12:25 +0800

    improve dev workflow with miniapp

--- npmScripts/dev.sh
+++ npmScripts/dev.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+npmScripts/killdevprocess.sh
 trap 'kill %1; kill %2; kill %3' SIGINT
 
 echo "*** start admin web application on port 8080"
@@ -7,17 +8,14 @@ cd admin/ &&
 npm run dev &
 
 echo "*** start meteor server on port 3000"
-pwd &&
 cd meteorServer/ &&
 meteor &
 
 echo "*** start wepy compile and watch"
-pwd &&
 cd miniapp/ &&
-wepy &
+wepy build --watch &
 
 echo "*** start miniapp by wept on port 8081"
-pwd &&
-sleep 5 &&
+sleep 15 ;
 cd miniapp/dist/ &&
 wept -p 8081
---
+++ npmScripts/killdevprocess.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+fuser -k 8080/tcp
+fuser -k 8081/tcp
+fuser -k 3000/tcp
